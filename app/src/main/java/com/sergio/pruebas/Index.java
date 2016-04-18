33979279ee9d8414b1ef26d7756254337ade6e42
Serulvan <Serulvan@gmail.com>
2016-04-18 17:47:44 +0200

    veo redes :D

--- app/src/main/java/com/sergio/pruebas/Index.java
+++ app/src/main/java/com/sergio/pruebas/Index.java
@@ -7,23 +7,26 @@ import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TableLayout;
 import android.widget.TextView;
 import android.net.*;
 import android.net.wifi.WifiManager;
+import android.widget.ToggleButton;
 
 public class Index extends AppCompatActivity implements View.OnClickListener{
     private TextView ssid, ip, masc, puerta, dns1, dns2;
     private Button btnShow;
+    private ToggleButton swButton;
     private TableLayout tl;
-    WifiManager wm;
+
+    private ListView lv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_index);
-        wm= (WifiManager) getSystemService(Context.WIFI_SERVICE);
-        wm.getDhcpInfo();
+
         ssid = (TextView)findViewById(R.id.ssid);
         ip = (TextView)findViewById(R.id.ip);
         masc = (TextView)findViewById(R.id.mascara);
@@ -31,9 +34,12 @@ public class Index extends AppCompatActivity implements View.OnClickListener{
         dns1 = (TextView)findViewById(R.id.dn1);
         dns2 = (TextView)findViewById(R.id.dn2);
         btnShow = (Button)findViewById(R.id.btnMostrar);
+        swButton = (ToggleButton)findViewById(R.id.swButton);
         tl = (TableLayout)findViewById(R.id.tlDatos);
+        lv=(ListView)findViewById(R.id.lv);
 
         btnShow.setOnClickListener(this);
+        swButton.setOnClickListener(this);
 
     }
 
@@ -46,15 +52,25 @@ public class Index extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     public void onClick(View v) {
-        ip.setText(intToIp(wm.getDhcpInfo().ipAddress));
-        masc.setText(intToIp(wm.getDhcpInfo().netmask));
-        puerta.setText(intToIp(wm.getDhcpInfo().gateway));
-        dns1.setText(intToIp(wm.getDhcpInfo().dns1));
-        dns2.setText(intToIp(wm.getDhcpInfo().dns2));
-        ssid.setText(wm.getConnectionInfo().getSSID());
-        tl.setVisibility(View.VISIBLE);
+        if (v.getId()==R.id.btnMostrar) {
+            WifiManager wm = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+            wm.getDhcpInfo();
+            ip.setText(intToIp(wm.getDhcpInfo().ipAddress));
+            masc.setText(intToIp(wm.getDhcpInfo().netmask));
+            puerta.setText(intToIp(wm.getDhcpInfo().gateway));
+            dns1.setText(intToIp(wm.getDhcpInfo().dns1));
+            dns2.setText(intToIp(wm.getDhcpInfo().dns2));
+            ssid.setText(wm.getConnectionInfo().getSSID());
 
-        WifiConfiguration wc = new WifiConfiguration();
+            lv.setAdapter(new testAdapter(this, R.layout.wifi_bar, wm.getScanResults()));
+        } else{
+            if (!swButton.isChecked()){
+                tl.setVisibility(View.GONE);
+            }else{
+                tl.setVisibility(View.VISIBLE);
+            }
+        }
+        //WifiConfiguration wc = new WifiConfiguration();
         //android.provider.Settings.System.putString(getContentResolver(), android.provider.Settings.
         //android.provider.Settings.System.putString(getContentResolver(), Settings.System.WIFI_USE_STATIC_IP,"1");
 
---
+++ app/src/main/java/com/sergio/pruebas/testAdapter.java
@@ -0,0 +1,50 @@
+package com.sergio.pruebas;
+
+import android.content.Context;
+import android.net.wifi.ScanResult;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+public class testAdapter extends ArrayAdapter {
+    private Context c;
+    private List<ScanResult> arr;
+    private int view;
+    public testAdapter(Context context, int resource, List<ScanResult> objects) {
+        super(context, resource, objects);
+        c=context;
+        arr=objects;
+        view=resource;
+
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View item = convertView;
+
+        if (item==null){
+            LayoutInflater inflater = (LayoutInflater)getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            item = inflater.inflate(view,null);
+        }
+        TextView ssid = (TextView)item.findViewById(R.id.ssid_list);
+        ssid.setText(arr.get(position).SSID);
+        TextView datos1 = (TextView)item.findViewById(R.id.datos1);
+        if (arr.get(position).capabilities.contains("WPA")) {
+            datos1.setText("WPA/WPA2 PSK");
+        }else{
+            datos1.setText(arr.get(position).capabilities);
+        }
+        TextView datos2 = (TextView)item.findViewById(R.id.datos2);
+        datos2.setText(String.valueOf(arr.get(position).level));
+        datos2.setVisibility(View.VISIBLE);
+
+        return item;
+    }
+}
