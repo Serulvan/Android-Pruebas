f9682de09850f7de382a083696cfd8fe97a9082b
Serulvan <Serulvan@gmail.com>
2016-04-28 17:31:53 +0200

    Servicios... que es eso, se come?

--- app/src/main/java/com/sergio/pruebas/hilos/HiloCompruebaEstado.java
+++ app/src/main/java/com/sergio/pruebas/hilos/HiloCompruebaEstado.java
@@ -1,18 +1,36 @@
 package com.sergio.pruebas.hilos;
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.AsyncTask;
+import android.widget.Toast;
 
 
 public class HiloCompruebaEstado extends AsyncTask<Void,Void,Void> {
+    private Context a;
+    private Boolean activo = true;
+
+    public HiloCompruebaEstado(Context a, Boolean activo) {
+        this.a = a;
+        this.activo = activo;
+    }
+
     @Override
     protected Void doInBackground(Void... params) {
-        try {
-            Thread.sleep(1000*60*5);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        while (activo) {
+            try {
+                Thread.sleep(1000 * 60 * 5);
+                publishProgress();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
         return null;
     }
 
+    @Override
+    protected void onProgressUpdate(Void... values) {
+        Toast.makeText(a, "hola", Toast.LENGTH_SHORT).show();
+    }
     //metodo buscar la red wifi mas alta(por encima de la intensidad de la señal actual)
 }
--- app/src/main/java/com/sergio/pruebas/servicio/Servicio.java
+++ app/src/main/java/com/sergio/pruebas/servicio/Servicio.java
@@ -1,18 +1,21 @@
-package com.sergio.pruebas;
+package com.sergio.pruebas.servicio;
 
+import android.app.Activity;
+import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 
 import com.sergio.pruebas.hilos.HiloCompruebaEstado;
 
-public class Service extends android.app.Service{
+public class Servicio extends Service {
 
     private HiloCompruebaEstado hce;
 
     @Override
     public void onCreate() {
         super.onCreate();
-
+        hce = new HiloCompruebaEstado(getBaseContext(),true);
+        hce.execute();
     }
 
     @Override
